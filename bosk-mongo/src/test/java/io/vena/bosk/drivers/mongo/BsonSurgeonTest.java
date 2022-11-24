a32c51d4642e0d3a67e69d1a48ad919804378a4c
Patrick Doyle <pdoyle@venacorp.com>
2022-11-24 08:08:45 -0500

    Initial experiment with scattering BSON

---
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
@@ -0,0 +1,80 @@
+package io.vena.bosk.drivers.mongo;
+
+import io.vena.bosk.Bosk;
+import io.vena.bosk.Catalog;
+import io.vena.bosk.CatalogReference;
+import io.vena.bosk.Path;
+import io.vena.bosk.Reference;
+import io.vena.bosk.drivers.AbstractDriverTest;
+import io.vena.bosk.drivers.state.TestEntity;
+import io.vena.bosk.exceptions.InvalidTypeException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import lombok.var;
+import org.bson.BsonBoolean;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.vena.bosk.drivers.mongo.Formatter.dottedFieldNameSegments;
+
+public class BsonSurgeonTest extends AbstractDriverTest {
+	BsonPlugin bsonPlugin;
+	Formatter formatter;
+
+	@BeforeEach
+	void setup() {
+		setupBosksAndReferences(Bosk::simpleDriver);
+		bsonPlugin = new BsonPlugin();
+		formatter = new Formatter(bosk, bsonPlugin);
+	}
+
+	@Test
+	void test() throws InvalidTypeException {
+		CatalogReference<TestEntity> separateCollectionRef = bosk.catalogReference(TestEntity.class,
+			Path.just("catalog"));
+		makeCatalog(separateCollectionRef);
+
+		BsonDocument entireDoc;
+		try (var __ = bosk.readContext()) {
+			entireDoc = (BsonDocument) formatter.object2bsonValue(bosk.rootReference().value(), bosk.rootReference().targetType());
+		}
+
+		Map<Path, BsonDocument> parts = scatter(separateCollectionRef, entireDoc);
+
+		parts.forEach((path, value) -> {
+			System.out.println(" Path: " + path);
+			System.out.println("Value: " + value.toJson());
+		});
+	}
+
+	@NotNull
+	private Map<Path, BsonDocument> scatter(Reference<?> separateCollectionRef, BsonDocument entireDoc) {
+		Map<Path, BsonDocument> parts = new LinkedHashMap<>();
+		ArrayList<String> segments = dottedFieldNameSegments(separateCollectionRef, bosk.rootReference());
+		BsonDocument docToSeparate = entireDoc;
+		for (String segment: segments.subList(1, segments.size())) {
+			docToSeparate = docToSeparate.getDocument(segment);
+		}
+		for (Map.Entry<String, BsonValue> entry: docToSeparate.entrySet()) {
+			parts.put(separateCollectionRef.path().then(entry.getKey()), (BsonDocument) entry.getValue());
+			entry.setValue(BsonBoolean.TRUE);
+		}
+
+		// entireDoc has now had the scattered pieces replaced by BsonBoolean.TRUE
+		parts.put(Path.empty(), entireDoc);
+
+		return parts;
+	}
+
+	private void makeCatalog(CatalogReference<TestEntity> ref) {
+		TestEntity child1 = autoInitialize(ref.then(child1ID));
+		TestEntity child2 = autoInitialize(ref.then(child2ID));
+
+		Catalog<TestEntity> bothChildren = Catalog.of(child1, child2);
+		driver.submitReplacement(ref, bothChildren);
+	}
+}
