20e49ebcec8bead2af218000323405c7f90c88aa
Patrick Doyle <pdoyle@venacorp.com>
2022-12-02 10:09:28 -0500

    Got nested catalogs working cleanly

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
@@ -11,13 +11,16 @@ import io.vena.bosk.exceptions.InvalidTypeException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.var;
 import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
 import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.codecs.Codec;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonWriterSettings;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,46 +44,59 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 	@Test
 	void test() throws InvalidTypeException {
 		CatalogReference<TestEntity> catalogRef = bosk.catalogReference(TestEntity.class, Path.just("catalog"));
-		List<Reference<?>> separateCollections = Arrays.asList(
-			catalogRef,
-			bosk.sideTableReference(TestEntity.class, TestEntity.class, Path.just("sideTable"))
+		CatalogReference<TestEntity> nestedCatalogRef = bosk.catalogReference(TestEntity.class, Path.of("catalog", "entity1", "catalog"));
+		List<CatalogReference<?>> separateCollections = Arrays.asList(
+			catalogRef, nestedCatalogRef
 		);
 		makeCatalog(catalogRef);
+		makeCatalog(nestedCatalogRef);
 
 		BsonDocument entireDoc;
 		try (var __ = bosk.readContext()) {
 			entireDoc = (BsonDocument) formatter.object2bsonValue(bosk.rootReference().value(), bosk.rootReference().targetType());
 		}
 
-		Map<Path, BsonValue> parts = scatter(separateCollections, entireDoc);
-		BsonDocument gathered = gather(parts);
+		BsonDocument parts = scatter(separateCollections, entireDoc.clone());
+		BsonDocument receivedParts = Document.parse(parts.toJson()).toBsonDocument(BsonDocument.class, new CodecRegistry() {
+			// Holy shit, this is awkward
+			@Override
+			public <T> Codec<T> get(Class<T> clazz) {
+				return (Codec<T>) formatter.codecFor(clazz);
+			}
+		});
+		BsonDocument gathered = gather(receivedParts);
 
 		assertEquals(entireDoc, gathered);
 
+		System.out.println("== Parts ==");
 		parts.forEach((path, value) -> {
 			System.out.println(" Path: " + path);
 			System.out.println("Value: " + value);
 		});
+		System.out.println("== Gathered ==");
+		System.out.println(gathered.toJson(JsonWriterSettings.builder().indent(true).build()));
 	}
 
+	/**
+	 * @param docToScatter will be modified!
+	 */
 	@NotNull
-	private Map<Path, BsonValue> scatter(Collection<Reference<?>> separateCollectionsArg, BsonDocument entireDoc) {
+	private BsonDocument scatter(Collection<? extends Reference<Catalog<?>>> separateCollectionsArg, BsonDocument docToScatter) {
 		List<Reference<?>> separateCollections = separateCollectionsArg.stream()
 			.sorted(comparing((Reference<?> ref) -> ref.path().length()).reversed())
 			.collect(toList());
-		BsonDocument docUnderConstruction = entireDoc.clone();
-		Map<Path, BsonValue> parts = new HashMap<>();
+		BsonDocument parts = new BsonDocument();
 		for (Reference<?> separateCollectionRef: separateCollections) {
 			ArrayList<String> segments = dottedFieldNameSegments(separateCollectionRef, bosk.rootReference());
-			BsonDocument docToSeparate = lookup(docUnderConstruction, segments.subList(1, segments.size()));
+			BsonDocument docToSeparate = lookup(docToScatter, segments.subList(1, segments.size()));
 			for (Map.Entry<String, BsonValue> entry: docToSeparate.entrySet()) {
-				parts.put(separateCollectionRef.path().then(entry.getKey()), entry.getValue());
+				parts.put(separateCollectionRef.path().then(entry.getKey()).urlEncoded(), entry.getValue());
 				entry.setValue(BsonBoolean.TRUE);
 			}
 		}
 
 		// docUnderConstruction has now had the scattered pieces replaced by BsonBoolean.TRUE
-		parts.put(Path.empty(), docUnderConstruction);
+		parts.put(Path.empty().urlEncoded(), docToScatter);
 
 		return parts;
 	}
@@ -93,8 +109,11 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 		return result;
 	}
 
-	private BsonDocument gather(Map<Path, BsonValue> parts) {
-		BsonDocument whole = (BsonDocument) parts.get(Path.empty());
+	/**
+	 * @param parts will be modified!
+	 */
+	private BsonDocument gather(BsonDocument parts) {
+		BsonDocument whole = ((BsonDocument) parts.get(Path.empty().urlEncoded()));
 
 		// Sorting by path length ensures we gather parents before children.
 		// (Sorting lexicographically might be better for cache locality.)
@@ -102,14 +121,14 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 		partsList.sort(comparing(entry -> entry.getKey().length()));
 
 		for (var entry: partsList) {
-			Path path = entry.getKey();
+			Path path = Path.parse(entry.getKey());
 			if (path.isEmpty()) {
 				// We're already merging everything into the main document. Skip the root entry.
 				continue;
 			}
 			List<String> containerSegments = path.truncatedBy(1).segmentStream().collect(toList());
 			BsonDocument container = lookup(whole, containerSegments);
-			BsonValue value = entry.getValue();
+			BsonValue value = (BsonValue) entry.getValue();
 
 			// The container should already have an entry. We'll be replacing it,
 			// and this does not affect the order of the entries.
