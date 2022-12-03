dfc4675ac81ce8bc38b0b11b84c3d2b61d1826b1
Patrick Doyle <pdoyle@venacorp.com>
2022-12-03 09:20:36 -0500

    Switch to recipe-based scatter/gather

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
@@ -67,36 +67,27 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 			entireDoc = (BsonDocument) formatter.object2bsonValue(bosk.rootReference().value(), bosk.rootReference().targetType());
 		}
 
-		BsonDocument parts = scatter(separateCollections, entireDoc.clone());
-		BsonDocument receivedParts = Document.parse(parts.toJson()).toBsonDocument(BsonDocument.class, new CodecRegistry() {
+		List<BsonDocument> parts = scatter(separateCollections, entireDoc.clone());
+		List<BsonDocument> receivedParts = parts.stream()
+			.map(part -> Document.parse(part.toJson()).toBsonDocument(BsonDocument.class, new CodecRegistry() {
 			// Holy shit, this is awkward
 			@Override
 			public <T> Codec<T> get(Class<T> clazz) {
 				return (Codec<T>) formatter.codecFor(clazz);
 			}
-		});
-		BsonDocument gathered = gather(receivedParts, bosk.rootReference());
+		})).collect(toList());
+		BsonDocument gathered = gather(receivedParts);
 
 		assertEquals(entireDoc, gathered);
 
 		JsonWriterSettings jsonWriterSettings = JsonWriterSettings.builder().indent(true).build();
 
 		System.out.println("== Parts ==");
-		parts.forEach((path, value) -> {
-			System.out.println(" Path: " + path);
-			System.out.println("Value: " + value);
+		parts.forEach(part -> {
+			System.out.println(part.toJson(jsonWriterSettings));
 		});
 		System.out.println("== Gathered ==");
 		System.out.println(gathered.toJson(jsonWriterSettings));
-
-		List<BsonDocument> parts2 = scatter2(separateCollections, entireDoc.clone());
-		System.out.println("== Parts2 ==");
-		parts2.forEach(part -> {
-			System.out.println(part.toJson(jsonWriterSettings));
-		});
-		BsonDocument gathered2 = gather2(parts2);
-		System.out.println("== Gathered2 ==");
-		System.out.println(gathered2.toJson(jsonWriterSettings));
 	}
 
 	/**
@@ -105,13 +96,13 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 	 * @param docToScatter will be modified!
 	 */
 	@NotNull
-	private List<BsonDocument> scatter2(List<Reference<? extends EnumerableByIdentifier<?>>> separateCollectionsArg, BsonDocument docToScatter) {
+	private List<BsonDocument> scatter(List<Reference<? extends EnumerableByIdentifier<?>>> separateCollectionsArg, BsonDocument docToScatter) {
 		List<Reference<?>> separateCollections = separateCollectionsArg.stream()
 			.sorted(comparing((Reference<?> ref) -> ref.path().length()).reversed())
 			.collect(toList());
 		List<BsonDocument> parts = new ArrayList<>();
 		for (Reference<?> collectionRef: separateCollections) {
-			scatterOneCollection2(collectionRef, docToScatter, parts);
+			scatterOneCollection(collectionRef, docToScatter, parts);
 		}
 
 		// docUnderConstruction has now had the scattered pieces replaced by BsonBoolean.TRUE
@@ -120,52 +111,7 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 		return parts;
 	}
 
-	/**
-	 * For efficiency, this modifies <code>docToScatter</code> in-place.
-	 *
-	 * @param docToScatter will be modified!
-	 */
-	@NotNull
-	private BsonDocument scatter(List<Reference<? extends EnumerableByIdentifier<?>>> separateCollectionsArg, BsonDocument docToScatter) {
-		List<Reference<?>> separateCollections = separateCollectionsArg.stream()
-			.sorted(comparing((Reference<?> ref) -> ref.path().length()).reversed())
-			.collect(toList());
-		BsonDocument parts = new BsonDocument();
-		for (Reference<?> collectionRef: separateCollections) {
-			scatterOneCollection(collectionRef, docToScatter, parts);
-		}
-
-		// docUnderConstruction has now had the scattered pieces replaced by BsonBoolean.TRUE
-		parts.put(Path.empty().urlEncoded(), docToScatter);
-
-		return parts;
-	}
-
-	private void scatterOneCollection(Reference<?> collectionRef, BsonDocument docToScatter, BsonDocument parts) {
-		Path path = collectionRef.path();
-		ArrayList<String> segments;
-		try {
-			segments = dottedFieldNameSegments(collectionRef.then(Object.class, "-id-"), bosk.rootReference());
-		} catch (InvalidTypeException e) {
-			throw new NotYetImplementedException(e);
-		}
-		if (path.numParameters() == 0) {
-			BsonDocument docToSeparate = lookup(docToScatter, segments.subList(1, segments.size()-1));
-			for (Map.Entry<String, BsonValue> entry: docToSeparate.entrySet()) {
-				parts.put(path.then(entry.getKey()).urlEncoded(), entry.getValue());
-				entry.setValue(BsonBoolean.TRUE);
-			}
-		} else {
-			// Loop through all possible values of the first parameter and recurse
-			int fpi = path.firstParameterIndex();
-			BsonDocument catalogDoc = lookup(docToScatter, segments.subList(1, fpi+1));
-			catalogDoc.forEach((id, value) -> {
-				scatterOneCollection(collectionRef.boundTo(Identifier.from(id)), docToScatter, parts);
-			});
-		}
-	}
-
-	private void scatterOneCollection2(Reference<?> collectionRef, BsonDocument docToScatter, List<BsonDocument> parts) {
+	private void scatterOneCollection(Reference<?> collectionRef, BsonDocument docToScatter, List<BsonDocument> parts) {
 		Reference<Object> entryRef;
 		try {
 			entryRef = collectionRef.then(Object.class, "-id-");
@@ -189,7 +135,7 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 			int fpi = path.firstParameterIndex();
 			BsonDocument catalogDoc = lookup(docToScatter, segments.subList(1, fpi + 1));
 			catalogDoc.forEach((id, value) -> {
-				scatterOneCollection2(collectionRef.boundTo(Identifier.from(id)), docToScatter, parts);
+				scatterOneCollection(collectionRef.boundTo(Identifier.from(id)), docToScatter, parts);
 			});
 		}
 	}
@@ -213,49 +159,11 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 	}
 
 	/**
-	 * For efficiency, this modifies <code>parts</code> in-place.
-	 *
-	 * @param parts will be modified!
-	 */
-	private BsonDocument gather(BsonDocument parts, Reference<?> rootRef) {
-		BsonDocument whole = ((BsonDocument) parts.get(Path.empty().urlEncoded()));
-
-		// Sorting by path length ensures we gather parents before children.
-		// (Sorting lexicographically might be better for cache locality.)
-		var partsList = new ArrayList<>(parts.entrySet());
-		partsList.sort(comparing(entry -> entry.getKey().length()));
-
-		for (var entry: partsList) {
-			Path path = Path.parse(entry.getKey());
-			if (path.isEmpty()) {
-				// We're already merging everything into the main document. Skip the root entry.
-				continue;
-			}
-			String[] pathSegments = path.segmentStream().toArray(String[]::new);
-			Reference<?> ref;
-			try {
-				ref = rootRef.then(Object.class, pathSegments);
-			} catch (InvalidTypeException e) {
-				throw new NotYetImplementedException(e);
-			}
-			List<String> bsonSegments = dottedFieldNameSegments(ref, rootRef);
-			BsonDocument container = lookup(whole, bsonSegments.subList(1, bsonSegments.size() - 1));
-			BsonValue value = entry.getValue();
-
-			// The container should already have an entry. We'll be replacing it,
-			// and this does not affect the order of the entries.
-			container.put(path.lastSegment(), value);
-		}
-
-		return whole;
-	}
-
-	/**
 	 * For efficiency, this modifies <code>partsList</code> in-place.
 	 *
 	 * @param partsList will be modified!
 	 */
-	private BsonDocument gather2(List<BsonDocument> partsList) {
+	private BsonDocument gather(List<BsonDocument> partsList) {
 		// Sorting by path length ensures we gather parents before children.
 		// (Sorting lexicographically might be better for cache locality.)
 		partsList.sort(comparing(doc -> doc.getArray("bsonPath").size()));
