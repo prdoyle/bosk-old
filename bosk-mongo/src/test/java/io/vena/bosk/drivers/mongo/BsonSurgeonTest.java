4e6886ebbded97fb5de16a92a367fa087fed87d9
Patrick Doyle <pdoyle@venacorp.com>
2022-12-03 09:00:08 -0500

    Scatter2

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/BsonSurgeonTest.java
@@ -17,8 +17,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import lombok.var;
+import org.bson.BsonArray;
 import org.bson.BsonBoolean;
 import org.bson.BsonDocument;
+import org.bson.BsonString;
 import org.bson.BsonValue;
 import org.bson.Document;
 import org.bson.codecs.Codec;
@@ -76,13 +78,42 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 
 		assertEquals(entireDoc, gathered);
 
+		JsonWriterSettings jsonWriterSettings = JsonWriterSettings.builder().indent(true).build();
+
 		System.out.println("== Parts ==");
 		parts.forEach((path, value) -> {
 			System.out.println(" Path: " + path);
 			System.out.println("Value: " + value);
 		});
 		System.out.println("== Gathered ==");
-		System.out.println(gathered.toJson(JsonWriterSettings.builder().indent(true).build()));
+		System.out.println(gathered.toJson(jsonWriterSettings));
+
+		List<BsonDocument> parts2 = scatter2(separateCollections, entireDoc.clone());
+		System.out.println("== Parts2 ==");
+		parts2.forEach(part -> {
+			System.out.println(part.toJson(jsonWriterSettings));
+		});
+	}
+
+	/**
+	 * For efficiency, this modifies <code>docToScatter</code> in-place.
+	 *
+	 * @param docToScatter will be modified!
+	 */
+	@NotNull
+	private List<BsonDocument> scatter2(List<Reference<? extends EnumerableByIdentifier<?>>> separateCollectionsArg, BsonDocument docToScatter) {
+		List<Reference<?>> separateCollections = separateCollectionsArg.stream()
+			.sorted(comparing((Reference<?> ref) -> ref.path().length()).reversed())
+			.collect(toList());
+		List<BsonDocument> parts = new ArrayList<>();
+		for (Reference<?> collectionRef: separateCollections) {
+			scatterOneCollection2(collectionRef, docToScatter, parts);
+		}
+
+		// docUnderConstruction has now had the scattered pieces replaced by BsonBoolean.TRUE
+		parts.add(createRecipe(new BsonArray(), docToScatter));
+
+		return parts;
 	}
 
 	/**
@@ -128,6 +159,41 @@ public class BsonSurgeonTest extends AbstractDriverTest {
 				scatterOneCollection(collectionRef.boundTo(Identifier.from(id)), docToScatter, parts);
 			});
 		}
+	}
+
+	private void scatterOneCollection2(Reference<?> collectionRef, BsonDocument docToScatter, List<BsonDocument> parts) {
+		Reference<Object> entryRef;
+		try {
+			entryRef = collectionRef.then(Object.class, "-id-");
+		} catch (InvalidTypeException e) {
+			throw new NotYetImplementedException(e);
+		}
+		ArrayList<String> segments = dottedFieldNameSegments(entryRef, bosk.rootReference());
+		Path path = collectionRef.path();
+		if (path.numParameters() == 0) {
+			List<String> containingDocSegments = segments.subList(1, segments.size() - 1);
+			BsonArray containingDocBsonPath = new BsonArray(containingDocSegments.stream().map(BsonString::new).collect(toList()));
+			BsonDocument docToSeparate = lookup(docToScatter, containingDocSegments);
+			for (Map.Entry<String, BsonValue> entry : docToSeparate.entrySet()) {
+				BsonArray entryBsonPath = containingDocBsonPath.clone();
+				entryBsonPath.add(new BsonString(entry.getKey()));
+				parts.add(createRecipe(entryBsonPath, entry.getValue()));
+				entry.setValue(BsonBoolean.TRUE);
+			}
+		} else {
+			// Loop through all possible values of the first parameter and recurse
+			int fpi = path.firstParameterIndex();
+			BsonDocument catalogDoc = lookup(docToScatter, segments.subList(1, fpi + 1));
+			catalogDoc.forEach((id, value) -> {
+				scatterOneCollection2(collectionRef.boundTo(Identifier.from(id)), docToScatter, parts);
+			});
+		}
+	}
+
+	private static BsonDocument createRecipe(BsonArray entryBsonPath, BsonValue entryState) {
+		return new BsonDocument()
+			.append("bsonPath", entryBsonPath)
+			.append("state", entryState);
 	}
 
 	private static BsonDocument lookup(BsonDocument entireDoc, List<String> segments) {
