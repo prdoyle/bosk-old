a3358a8684001c425de9666c7d249a8bcf400d03
Patrick Doyle <pdoyle@venacorp.com>
2023-02-19 09:09:23 -0500

    Add _multi test configuration

--- bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
+++ bosk-mongo/src/test/java/io/vena/bosk/drivers/mongo/TestParameters.java
@@ -1,11 +1,13 @@
 package io.vena.bosk.drivers.mongo;
 
+import io.vena.bosk.Path;
 import io.vena.bosk.drivers.mongo.MongoDriverSettings.MongoDriverSettingsBuilder;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static io.vena.bosk.drivers.mongo.MongoDriverSettings.FlushMode.ECHO;
 import static io.vena.bosk.drivers.mongo.MongoDriverSettings.FlushMode.REVISION_FIELD_ONLY;
+import static java.util.Collections.singletonList;
 
 public interface TestParameters {
 	AtomicInteger dbCounter = new AtomicInteger(0);
@@ -17,6 +19,9 @@ public interface TestParameters {
 			MongoDriverSettings.builder()
 				.database(prefix + "_echo")
 				.flushMode(ECHO),
+			MongoDriverSettings.builder()
+				.database(prefix + "_multi")
+				.separateCollections(singletonList(Path.just("catalog"))),
 			MongoDriverSettings.builder()
 				.database(prefix + "_rev")
 				.flushMode(REVISION_FIELD_ONLY)
