dd4e8930b727d46e99d91efa89f3a156063619c1
Patrick Doyle <pdoyle@venacorp.com>
2023-05-02 09:36:23 -0400

    Refactor: reformat DynamicDriverFacade

--- bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/modal/DynamicDriverFacade.java
+++ bosk-mongo/src/main/java/io/vena/bosk/drivers/mongo/modal/DynamicDriverFacade.java
@@ -39,13 +39,48 @@ public final class DynamicDriverFacade<R extends Entity> implements MongoDriver<
 		return this.current.compareAndSet(from, to);
 	}
 
-	@Override public R initialRoot(Type rootType) throws InvalidTypeException, IOException, InterruptedException { return current.get().initialRoot(rootType); }
-	@Override public <T> void submitReplacement(Reference<T> target, T newValue) { current.get().submitReplacement(target, newValue); }
-	@Override public <T> void submitConditionalReplacement(Reference<T> target, T newValue, Reference<Identifier> precondition, Identifier requiredValue) { current.get().submitConditionalReplacement(target, newValue, precondition, requiredValue); }
-	@Override public <T> void submitInitialization(Reference<T> target, T newValue) { current.get().submitInitialization(target, newValue); }
-	@Override public <T> void submitDeletion(Reference<T> target) { current.get().submitDeletion(target); }
-	@Override public <T> void submitConditionalDeletion(Reference<T> target, Reference<Identifier> precondition, Identifier requiredValue) { current.get().submitConditionalDeletion(target, precondition, requiredValue); }
-	@Override public void flush() throws IOException, InterruptedException { current.get().flush(); }
-	@Override public void refurbish() { current.get().refurbish(); }
-	@Override public void close() { current.get().close(); }
+	@Override
+	public R initialRoot(Type rootType) throws InvalidTypeException, IOException, InterruptedException {
+		return current.get().initialRoot(rootType);
+	}
+
+	@Override
+	public <T> void submitReplacement(Reference<T> target, T newValue) {
+		current.get().submitReplacement(target, newValue);
+	}
+
+	@Override
+	public <T> void submitConditionalReplacement(Reference<T> target, T newValue, Reference<Identifier> precondition, Identifier requiredValue) {
+		current.get().submitConditionalReplacement(target, newValue, precondition, requiredValue);
+	}
+
+	@Override
+	public <T> void submitInitialization(Reference<T> target, T newValue) {
+		current.get().submitInitialization(target, newValue);
+	}
+
+	@Override
+	public <T> void submitDeletion(Reference<T> target) {
+		current.get().submitDeletion(target);
+	}
+
+	@Override
+	public <T> void submitConditionalDeletion(Reference<T> target, Reference<Identifier> precondition, Identifier requiredValue) {
+		current.get().submitConditionalDeletion(target, precondition, requiredValue);
+	}
+
+	@Override
+	public void flush() throws IOException, InterruptedException {
+		current.get().flush();
+	}
+
+	@Override
+	public void refurbish() {
+		current.get().refurbish();
+	}
+
+	@Override
+	public void close() {
+		current.get().close();
+	}
 }
